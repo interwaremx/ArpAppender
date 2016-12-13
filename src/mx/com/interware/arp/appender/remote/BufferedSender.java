5bd415496096277ccfb9e460c452f5a35333550d
Jorge Esteban <jzaragoza@interware.com.mx>
2016-12-13 11:51:15 -0600

    Version para java6/java7
    Se hacen cambios para hacer compatible java6/java7

--- src/mx/com/interware/arp/appender/remote/BufferedSender.java
+++ src/mx/com/interware/arp/appender/remote/BufferedSender.java
@@ -28,17 +28,20 @@ public class BufferedSender {
     private String ednFormat;
 
     /**
-     * 
+     *
      * Constructor que inicializa los atributos de la clase y levanta un worker
      * que se reconecta al tener un error o desconexión.
+     *
      * @param timestampNow timestamp en milisegundos del tiempo de la operación.
-     * @param socket Socket persistente por el cuál se realizará la comunicación.
+     * @param socket Socket persistente por el cuál se realizará la
+     * comunicación.
      * @param delta tiempo de espera de renvío en milisegundos.
      * @param maxQueue numero máximo de elementos en espera de envío.
      * @param regexp expresión regular para comprobar los mensajes del log.
      * @param ednFormat formato del edn de retorno
-     * 
-     **/
+     *
+     *
+     */
     public BufferedSender(Long timestampNow,
             PersistentSocket socket,
             Long delta,
@@ -48,22 +51,23 @@ public class BufferedSender {
         BufferedSender.timestampNow = timestampNow;
         this.socket = socket;
         this.delta = delta;
-        this.linesLst = new CopyOnWriteArrayList<>();
+        this.linesLst = new CopyOnWriteArrayList<Map>();
         this.maxQueue = maxQueue;
         this.regexp = regexp;
         this.ednFormat = ednFormat;
-
+        final PersistentSocket socketThread = socket;
+        final Long deltaThread = delta;
         Thread senderWorker = new Thread() {
             @Override
             public void run() {
                 while (true) {
                     try {
-                        if (BufferedSender.timestampNow < (Calendar.getInstance().getTimeInMillis() + delta)
-                                && socket.getIsConnected()
+                        if (BufferedSender.timestampNow < (Calendar.getInstance().getTimeInMillis() + deltaThread)
+                                && socketThread.getIsConnected()
                                 && linesLst.size() > 0) {
                             sendMessage();
                         } else {
-                            sleep(delta);
+                            sleep(deltaThread);
                         }
                     } catch (InterruptedException ex) {
                         Logger.getLogger(BufferedSender.class.getName()).log(Level.SEVERE, null, ex);
@@ -73,13 +77,14 @@ public class BufferedSender {
         };
         senderWorker.start();
     }
-    
+
     /**
      * Método que agrega a la cola de envío un evento del appender
+     *
      * @param event evento del logging obtenido desde el appender
      */
     public synchronized void add(LoggingEvent event) {
-        Map<String, String> data = new HashMap<>();
+        Map<String, String> data = new HashMap<String, String>();
         data.put("thread", event.getThreadName());
         data.put("level", event.getLevel().toString());
         data.put("timestamp", String.valueOf(event.getTimeStamp()));
@@ -91,19 +96,19 @@ public class BufferedSender {
         }
 
     }
-    
+
     /**
      * Método que envía y limpia la cola de envío al socket
      */
     private void sendMessage() {
-        List<Map> envio = new CopyOnWriteArrayList<>();
-        linesLst.stream().forEach((mapa) -> {
+        List<Map> envio = new CopyOnWriteArrayList<Map>();
+         for(Map mapa : linesLst){
             envio.add(mapa);
-        });
+        }
         this.flush();
         socket.sendMessage(EdnUtil.createEDN(envio, regexp, ednFormat));
     }
-    
+
     /**
      * Método que limpia la cola de envío
      */
